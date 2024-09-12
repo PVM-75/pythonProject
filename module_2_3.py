3c9c4c033c52c9ec477f80ace59ec4bdb6ea4b2a
pvm <vperevalov@gmail.com>
2024-09-12 15:33:03 +0800

    initial

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index_list = 0
+while index_list < len(my_list):
+    if my_list[index_list] == 0:
+        index_list = index_list + 1
+        # continue
+    elif my_list[index_list] > 0:
+        print(my_list[index_list])
+        index_list = index_list + 1
+    else:
+        break
