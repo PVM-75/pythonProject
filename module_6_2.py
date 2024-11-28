8d92828dd8153893081d76c67ac12040c5a9be72
pvm <vperevalov@gmail.com>
2024-11-28 14:06:03 +0800

    new edition

--- module_6_2.py
+++ module_6_2.py
@@ -1,5 +1,4 @@
 class Vehicle:
-    color_variants = ["blue", "green", "black", "white"]
     def __init__(self, owner, model, color, engine_power):
         self.owner = owner
         self.__model = model
@@ -21,6 +20,19 @@ class Vehicle:
         print(f"Цвет: {self.color}")
         print(f"Владелец: {self.owner}")
 
+    def set_color(self, new_color):
+        __color_variants = ["blue", "green", "black", "white"]
+        sum_of_colors = len(__color_variants)
+        for i in __color_variants:
+            sum_of_colors -= 1
+            if new_color.lower() == i:
+                self.color = new_color.lower()
+                print(f'У {self.__model} сменили цвет на {new_color.lower()}')
+                break # без него выдает сообщение "Нельзя сменить цвет.." каждый раз, даже при удачной смене цвета
+            elif new_color.lower() != i and sum_of_colors == 0:
+                print(f'Нельзя сменить цвет на {new_color}')
+
+
 class Sedan(Vehicle):
     def __init__(self, owner, model, color, engine_power):
         super().__init__(owner, model, color, engine_power)
@@ -33,12 +45,12 @@ if __name__ == "__main__":
     vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 
     # Изначальные свойства
-    # vehicle1.print_info()
+    vehicle1.print_info()
 
     # Меняем свойства (в т.ч. вызывая методы)
-    # vehicle1.set_color('Pink')
-    # vehicle1.set_color('BLACK')
-    # vehicle1.owner = 'Vasyok'
+    vehicle1.set_color('Pink')
+    vehicle1.set_color('BLACK')
+    vehicle1.owner = 'Vasyok'
 
     # Проверяем что поменялось
     vehicle1.print_info()
