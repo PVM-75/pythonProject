5d34d047a6d76873176f9a79ebdad6218a9497fb
pvm <vperevalov@gmail.com>
2024-12-02 17:26:20 +0800

    new edition

---
+++ module_5_hard.py
@@ -0,0 +1,19 @@
+class User:
+    def __init__(self, nickname, password, age):
+        self.nickname = nickname
+        self.password = hash(password)
+        self.age = age
+
+class Video:
+    def __init__(self, title, duration, time_now = 0, adult_mode = False):
+        self.title = title
+        self.duration = duration
+        self.time_now = time_now
+        self.adult_mode = adult_mode
+
+class UrTube:
+    pass
+
+if __name__ == '__main__':
+    vid1 = Video('Борька ест кашу', 250, 14, True)
+    print(vid1.time_now)
